f921157659d258bef3844fa6cc92d122ba658571
Lolade Ilori <loladeilori02@gmail.com>
2023-04-20 18:20:49 +0100

    desktop Ui done

--- .next/static/webpack/webpack.376201774a92edd9.hot-update.js
+++ .next/static/webpack/webpack.376201774a92edd9.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "6f77fb220f8ce9c8"; }
+/******/ 	__webpack_require__.h = function() { return "d11a494157929082"; }
 /******/ }();
 /******/ 
 /******/ }
