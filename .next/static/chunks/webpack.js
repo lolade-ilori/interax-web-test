f074b94081e9b8e2b1d8851a6cabf805e5d43d4d
Oluseyi Komolafe <koluseyi44@gmail.com>
2023-05-07 23:06:09 +0100

    fixed waitlist and font issue.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66ac29e92e3044ad"; }
+/******/ 		__webpack_require__.h = function() { return "0ca00825129e14cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
