c295d938bbb6936a7deed2df36b351958e83bd61
Lolade Ilori <loladeilori02@gmail.com>
2023-05-08 22:43:28 +0100

    bg color change

--- src/components/styled/blog.styles.ts
+++ src/components/styled/blog.styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import { HeaderText } from './team.styles'
 
 export const BlogOverall = styled.section `
-    background-color: #EEEEEE;
+    background-color: #fff;
     width: 100%;
     height: 100%;
 `
